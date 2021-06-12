2fb2f121cffffd7ba35339ed8caeec9b63b086c3
Rafa Gómez <rgomezcasas@gmail.com>
2021-06-12 11:20:22 +0200

    feat: self lint and static analysis also include users dotfiles files

--- scripts/core/dotly.sh
+++ scripts/core/dotly.sh
@@ -1,4 +1,9 @@
 dotly::list_bash_files() {
   grep "#!/usr/bin/env bash" "$DOTLY_PATH"/{bin,dotfiles_template,scripts,shell,installer,restorer} -R | awk -F':' '{print $1}'
   find "$DOTLY_PATH"/{bin,dotfiles_template,scripts,shell} -type f -name "*.sh"
+
+  if [[ -n ${DOTFILES_PATH+x} ]]; then
+    grep "#!/usr/bin/env bash" "$DOTFILES_PATH"/{bin,scripts,shell} -R | awk -F':' '{print $1}'
+    find "$DOTFILES_PATH"/{bin,scripts,shell} -type f -name "*.sh"
+  fi
 }
